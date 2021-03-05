2ed721c8f955d08d006ec1cc94010af33fa68f33
Gerald Onyango <futuregerald@Geralds-MacBook-Pro.local>
2021-03-05 10:00:31 -0500

    changed metadata to map of empty interface

--- src/functions/db/db.go
+++ src/functions/db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"encoding/json"
 	"log"
 
 	"github.com/kamva/mgm/v3"
@@ -12,15 +11,15 @@ import (
 type User struct {
 	// DefaultModel add _id,created_at and updated_at fields to the Model
 	mgm.DefaultModel `bson:",inline"`
-	Email            string             `json:"name" bson:"name"`
-	Tenant           primitive.ObjectID `json:"tenantID" bson:"tenantID,omitempty"`
-	Password         string             `json:"password" bson:"password"`
-	appMetaData      json.RawMessage    `json:"appMetaData" bson:"appMetaData"`
-	userMetaData     json.RawMessage    `json:"userMetaData" bson:"appMetaData"`
-	confirmed        bool               `json:"confirmed" bson:"confirmed"`
-	isAdmin          bool               `json:"isAdmin" bson:"isAdmin"`
-	disabled         bool               `json:"disabled" bson:"disabled"`
-	roles            []string           `json:"roles" bson:"roles"`
+	Email            string                 `json:"name" bson:"name"`
+	Tenant           primitive.ObjectID     `json:"tenantID" bson:"tenantID,omitempty"`
+	Password         string                 `json:"password" bson:"password"`
+	AppMetaData      map[string]interface{} `json:"appMetaData" bson:"appMetaData"`
+	UserMetaData     map[string]interface{} `json:"userMetaData" bson:"appMetaData"`
+	Confirmed        bool                   `json:"confirmed" bson:"confirmed"`
+	IsAdmin          bool                   `json:"isAdmin" bson:"isAdmin"`
+	Disabled         bool                   `json:"disabled" bson:"disabled"`
+	Roles            []string               `json:"roles" bson:"roles"`
 }
 
 // TODO populate NewUser method as per User struct
